6d85681fc3243c80d1983f74294be53f3c57447c
liberationfront <bomengeduld@gmail.com>
2017-10-18 19:37:44 +0200

    update
    ...

---
+++ _site/js/scrolling-nav.js
@@ -0,0 +1,16 @@
+
+//jQuery for page scrolling feature - requires jQuery Easing plugin
+$(function() {
+    $(document).on('click', 'a.page-scroll', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
+
+
+$('.nav a').on('click', function() {
+    $('.navbar-collapse').collapse('hide');
+});
